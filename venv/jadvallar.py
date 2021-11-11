0c8ae672437731031a8993bf5c26e8101cce5a80
shohruh17 <vokhobjonovshohrux@gmail.com>
2021-11-11 16:31:29 +0500

    if elif else funksiyalri

---
+++ venv/jadvallar.py
@@ -0,0 +1,10 @@
+yosh = int(input("yoshingizni kiriting "))
+if yosh <= 5 or yosh >= 60 :
+    price = o
+elif yosh <= 12 :
+    price = 5000
+elif yosh <= 20 :
+    price =10000
+else:
+    price = 15000
+    print(f"sizga kirish {price} som")
