0dadc9076bb78d3135fe92ab32768df59331b018
shohruh17 <vokhobjonovshohrux@gmail.com>
2021-11-11 16:16:27 +0500

    fanlar uchun dastur

---
+++ venv/oraliq baholar.py
@@ -0,0 +1,5 @@
+fizika = int(input(" fizikidan bahoingizni kiriting "))
+kimyo = int(input(" kimyodan bahoingizni kiriting "))
+tarix = int(input(" tarixdan bahoingizni kiriting "))
+ingliz_tili = int(input("ingliz tilidan bahoingizni kiriting "))
+print(f" sizning yillik ortacha bahoingiz {(fizika+kimyo+tarix+ingliz_tili)/4}")
